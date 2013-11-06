d549f2f361cdf6d199418eaf51eb0a220aafc097
joern.flath <joern.flath@gmail.com>
2013-11-06 11:01:34 +0100

    Erster Commit

---
+++ messeapp/src/de/unimannheim/template/MainActivity.java
@@ -0,0 +1,15 @@
+package de.unimannheim.template;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
